f789a6630945e8e2fa11621451127849edd7b272
Ratnesh <ratcancode@gmail.com>
2024-06-02 01:24:36 +0530

    Add project files.

---
+++ KajalEnterprises/Controllers/CategoryController.cs
@@ -0,0 +1,41 @@
+﻿using KajalEnterprises.Data;
+using KajalEnterprises.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace KajalEnterprises.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+        public CategoryController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+
+        
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+            
+        }
+
+    }
+}
