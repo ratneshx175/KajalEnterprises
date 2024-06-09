ad73ecd6818bf3089112e016642e549d274cc520
Ratnesh <ratcancode@gmail.com>
2024-06-10 02:04:42 +0530

    Worked on IndexPage

--- KajalEnterprises/Areas/Customer/Controllers/HomeController.cs
+++ KajalEnterprises/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,12 @@
-﻿using KajalEnterprises.Models;
+﻿using KajalEnterprises.Data;
+using KajalEnterprises.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Diagnostics;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace KajalEnterprises.Areas.Customer.Controllers
 {
@@ -8,18 +14,63 @@ namespace KajalEnterprises.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDBContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDBContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? categoryId)
+        {
+            var categories = _db.Categories.ToList();
+            ViewBag.Categories = new SelectList(categories, "Id", "Name");
+
+            List<Product> productList;
+            if (categoryId.HasValue && categoryId.Value > 0)
+            {
+                productList = _db.Products
+                    .Include(p => p.Category)
+                    .Where(p => p.CategoryId == categoryId.Value)
+                    .ToList();
+            }
+            else
+            {
+                productList = _db.Products
+                    .Include(p => p.Category)
+                    .ToList();
+            }
+
+            return View(productList);
+        }
+
+
+        public IActionResult Details(int id)
+        {
+            var product = _db.Products
+                .Include(p => p.Category)
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Privacy()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
         {
             return View();
         }
